3ad6ad585a9dbb763cf2672686cec02a9b01781f
Davide Cardillo <davix.cardillo@gmail.com>
2021-12-24 22:32:30 +0100

    Risolto problema di integrità

--- Crud-Film/cancella_regista.php
+++ Crud-Film/cancella_regista.php
@@ -1,27 +1,25 @@
-
 <?
 	include "connessione.php";
     $IDRegista = $_GET['IDRegista'];
-    $controllo = "SELECT * FROM `Film` WHERE `IDRegista` = ". $IDRegista . ";" ;
+    $controllo = "SELECT * FROM Film WHERE IDRegista =  $IDRegista;";
     $risultati = mysqli_query($connessione, $controllo);
     if (!$risultati) {
         echo("Errore nell'esecuzione della query: " . $connessione->error . ".");
+        echo $controllo;
         exit();
     } else {
         if ($risultati->num_rows > 0) {
-            header("Location: http://cardillodavide.altervista.org/Crud-Film/elenca_registi.php");
-            echo "<script type='text/javascript''>window.alert('Ci sono dei film! Elimina i film prima del regista');</script>";
+            echo "Il regista ha dei film collegati a lui! Eliminali prima di eliminare il regista?";
+            echo "<br><button type='button' class='btn btn-success' onclick=location.href='http://cardillodavide.altervista.org/Crud-Film/elenca_registi.php'>Torna alla home page</button> ";
         } else {
             $query = "DELETE FROM `Registi` WHERE `IDRegista`= " . $IDRegista . ";";
-            //echo $query."<br>";
             $result = mysqli_query($connessione, $query);
             if (!$result)
-                echo "window.alert('ERRORE');";
+                echo '<script>alert("Eliminato")</script>';
             else {
                 header('Location: http://cardillodavide.altervista.org/Crud-Film/elenca_registi.php');
-
-                echo "window.alert('eliminato!');";
             }
         }
+
     }
 ?>
