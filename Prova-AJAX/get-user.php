bf950d0d01a2915194fd88423716bcf2e3750203
Davide Cardillo <davix.cardillo@gmail.com>
2022-01-27 16:31:36 +0100

    Prima prova AJAX

---
+++ Prova-AJAX/get-user.php
@@ -0,0 +1,26 @@
+<?php
+include "connessione.php";
+$id = $_GET['id'];
+$la_query="select first_name, last_name, email, phone from users where id = $id ;";
+if(!$risultati=$connessione->query($la_query))
+{
+
+}
+while($recordset = $risultati->fetch_array(MYSQLI_ASSOC)){
+    $first_name = $recordset['first_name'];
+    $last_name = $recordset['last_name'];
+    $email = $recordset['email'];
+    $phone = $recordset['phone'];
+}
+echo "<table>";
+echo "<tr>";
+echo "<th>Nome</th>";
+echo "<td>" . $first_name . "</td>";
+echo "<th>Cognome</th>";
+echo "<td>" . $last_name . "</td>";
+echo "<th>Email</th>";
+echo "<td>" . $email . "</td>";
+echo "<th>Telefono</th>";
+echo "<td>" . $phone . "</td>";
+echo "</tr>";
+echo "</table>";
